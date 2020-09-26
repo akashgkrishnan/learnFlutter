1a2edcc030bd8676fc50eb66d72e51a20d2586f7
Akash <krishnanag1996@gmail.com>
2020-09-26 15:43:07 +0530

    done expenses

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -16,6 +17,10 @@ class MyHomePage extends StatelessWidget {
     Transaction(id: '1', title: 'Shoes', amount: 100, date: DateTime.now()),
     Transaction(id: '2', title: 'Shirts', amount: 90, date: DateTime.now()),
   ];
+
+  final inputTitle = TextEditingController();
+  final inputAmount = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +30,7 @@ class MyHomePage extends StatelessWidget {
         ),
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Card(
@@ -40,36 +45,88 @@ class MyHomePage extends StatelessWidget {
               ),
               elevation: 5,
             ),
+            Card(
+              elevation: 5,
+              child: Container(
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        TextField(
+                          // onChanged: (value) {
+                          //   inputTitle = value;
+                          // },
+                          controller : inputTitle,
+                          decoration: InputDecoration(
+                              labelText: 'What did you spend on'),
+                        ),
+                        TextField(
+                          // onChanged: (value) {
+                          //   inputAmount = value;
+                          // },
+                          controller: inputAmount,
+                          decoration: InputDecoration(
+                              labelText: 'How much did you spend?'),
+                        ),
+                        FlatButton(
+                          child: Text('Add Expense'),
+                          onPressed: () {
+                            print(inputTitle.text);
+                            print(inputAmount.text);
+                          },
+                          textColor: Colors.purple,
+                        ),
+                      ])),
+            ),
             Column(
               children: transactions.map((txn) {
                 return Card(
                   child: Row(
                     children: <Widget>[
                       Container(
+                        margin: EdgeInsets.all(10),
                         padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.all(15),
-                        child: Text(txn.amount.toString()),
+                        child: Text(
+                          '\$ ${txn.amount.toString()}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
                         decoration: BoxDecoration(
                           border: Border.all(
-                            color: Colors.black,
+                            color: Colors.purple,
                             width: 2,
                           ),
                         ),
                       ),
-                      Column(children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          margin: EdgeInsets.all(10),
-                          child: Text(txn.title),
-                        ),
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          margin: EdgeInsets.all(10),
-                          child: Text(
-                            txn.date.toString(),
-                            style: TextStyle(fontWeight: FontWeight.bold)),
-                        ),
-                      ])
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(5),
+                              child: Text(
+                                txn.title,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(5),
+                              child: Text(
+                                  DateFormat('dd-MMM-yyyy').format(txn.date),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                    color: Colors.black,
+                                  )),
+                            ),
+                          ])
                     ],
                   ),
                 );
