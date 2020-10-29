9831784bd5fb13961bd40fe7880c49f2bef1ea0d
Akash <krishnanag1996@gmail.com>
2020-10-29 14:11:18 +0530

    deleted unwanted apps

--- onboard_screen/lib/routes.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'screens/splash/splash_screen.dart';
-import 'screens/sign_in/sign_in_screen.dart';
-
-final Map<String, WidgetBuilder> routes = {
-  SplashScreen.routeName: (context) => SplashScreen(),
-  SignInScreen.routeName: (context) => SignInScreen(),
-};
--- onboard_screen/lib/routs.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'screens/splash/splash_screen.dart';
-
-final Map<String, WidgetBuilder> routes = {
-  SplashScreen.routeName: (context) => SplashScreen(),
-};
